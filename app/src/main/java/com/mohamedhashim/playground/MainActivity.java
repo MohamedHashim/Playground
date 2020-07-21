0f061ce6805f360b5a47552008036a26ca8ec3dd
MohamedHashim <sawmido2000@gmail.com>
2020-07-21 18:37:55 +0200

    Update gradle to version4

---
+++ app/src/main/java/com/mohamedhashim/playground/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mohamedhashim.playground;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
